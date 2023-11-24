d13ba682f4be8ab2b241620fd8dd9ac1eaf48b46
p21nc3 <pp4768@srmist.edu.in>
2023-11-25 01:51:49 +0530

    credentials placeholder

---
+++ credentials.js
@@ -0,0 +1,8 @@
+/*
+ My credentials.js file:
+*/
+const CLIENT_ID = "CLIENT_ID";
+const CLIENT_SECRET = "CLIENT_SECRET";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="REFRESH_TOKEN";
+module.exports = { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
